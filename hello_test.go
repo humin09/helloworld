d7e3a2a477b6bebd56487e55dc63305135a9a4c1
humin <humin09@163.com>
2020-05-16 09:24:23 +0800

    remove hello1

--- hello_test.go
+++ hello_test.go
@@ -15,19 +15,6 @@ func Test_hello(t *testing.T) {
 	}
 }
 
-func Test_hello1(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			hello1()
-		})
-	}
-}
-
 func Test_world(t *testing.T) {
 	tests := []struct {
 		name string
